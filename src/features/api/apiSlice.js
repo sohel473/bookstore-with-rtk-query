ff7be776d9c1a1fb339990050e301e5d662e67a8
Sohel <absohel47@gmail.com>
2023-03-18 12:35:21 +0000

    api slice done

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  endpoints: (builder) => ({
+    getBooks: builder.query({
+      query: () => "/books",
+    }),
+    getBook: builder.query({
+      query: (id) => `/books/${id}`,
+    }),
+    createBook: builder.mutation({
+      query: (body) => ({
+        url: "/books",
+        method: "POST",
+        body,
+      }),
+    }),
+    updateBook: builder.mutation({
+      query: (body) => ({
+        url: `/books/${body.id}`,
+        method: "PUT",
+        body,
+      }),
+    }),
+    deleteBook: builder.mutation({
+      query: (id) => ({
+        url: `/books/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetBooksQuery,
+  useGetBookQuery,
+  useCreateBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = apiSlice;
